879214c01ca27aa9ef26f3683be95d2f1e2ca6d2
Wahyu Gugus Nurcahyo <wahyugnc@gmail.com>
2022-09-04 07:51:02 +0700

    add kubectl deployement

--- db.sql
+++ db.sql
@@ -1,10 +1,6 @@
-create database usedeal_db;
-
 create or replace table users
 (
-    id         int auto_increment
-        constraint `PRIMARY`
-        primary key,
+    id         int auto_increment primary key,
     username   varchar(100) not null,
     passwd     varchar(250) null,
     role       varchar(20)  null,
@@ -14,5 +10,5 @@ create or replace table users
     updated_at datetime     null
 );
 
-INSERT INTO usedeall_db.users (id, username, passwd, role, first_name, last_name, created_at, updated_at) VALUES (10, 'wahyugnc', '$2a$10$KlYoceFmyBv9Zr23z/IB7eV5IEUvJTSSDI4o6aTSuVOVv1wX0qOM6', 'admin', 'Wahyu', 'Gugus Nurcahyo', '2022-09-03 08:05:22', null);
-INSERT INTO usedeall_db.users (id, username, passwd, role, first_name, last_name, created_at, updated_at) VALUES (11, 'nabil123', '$2a$10$SOfVd0n12JXuq3crJw9Jfek6qZeTTbm7BLJXr9tJQujvG0cnAybx.', 'user', 'Nabil', 'Kastara', '2022-09-03 08:06:31', null);
+INSERT INTO users (id, username, passwd, role, first_name, last_name, created_at, updated_at) VALUES (10, 'wahyugnc', '$2a$10$KlYoceFmyBv9Zr23z/IB7eV5IEUvJTSSDI4o6aTSuVOVv1wX0qOM6', 'admin', 'Wahyu', 'Gugus Nurcahyo', '2022-09-03 08:05:22', null);
+INSERT INTO users (id, username, passwd, role, first_name, last_name, created_at, updated_at) VALUES (11, 'nabil123', '$2a$10$SOfVd0n12JXuq3crJw9Jfek6qZeTTbm7BLJXr9tJQujvG0cnAybx.', 'user', 'Nabil', 'Kastara', '2022-09-03 08:06:31', null);
